82bfbca2769a52e7e69f2abb033b456f1fde820b
송동준 <dongjun569@naver.com>
2025-10-17 16:05:24 +0900

    기본 기능 개발: 게시글 CRUD, admin 등록, 템플릿 추가

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, UserProfile
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("id", "title", "author", "created_at", "updated_at")
+	search_fields = ("title", "content", "author__username")
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ("id", "user", "created_at")
+	search_fields = ("user__username",)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from django.http import HttpResponse
+
+def post_list(request):
+	posts = Post.objects.all().order_by('-created_at')
+	return render(request, 'core/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'core/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		content = request.POST.get('content')
+		post = Post.objects.create(author=request.user, title=title, content=content)
+		return redirect('post_detail', pk=post.pk)
+	return render(request, 'core/post_form.html')
+
+@login_required
+def post_update(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.user != post.author:
+		return HttpResponse('권한이 없습니다.', status=403)
+	if request.method == 'POST':
+		post.title = request.POST.get('title')
+		post.content = request.POST.get('content')
+		post.save()
+		return redirect('post_detail', pk=post.pk)
+	return render(request, 'core/post_form.html', {'post': post})
+
+@login_required
+def post_delete(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.user != post.author:
+		return HttpResponse('권한이 없습니다.', status=403)
+	if request.method == 'POST':
+		post.delete()
+		return redirect('post_list')
+	return render(request, 'core/post_confirm_delete.html', {'post': post})
 
 # Create your views here.
